5cb882e36884f49f70a61b059237f60b37ec92df
Michael <michaelcruz552@gmail.com>
2026-04-10 12:13:29 +0800

    feat: add CORS for frontend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 import fastapi
 from app.router import length, temperature, weight
+from fastapi.middleware.cors import CORSMiddleware
 
 app = fastapi.FastAPI()
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(length.router)
 app.include_router(weight.router)
