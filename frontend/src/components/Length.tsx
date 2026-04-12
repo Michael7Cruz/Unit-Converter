f71995cb10f37598856b04621a685019b7723961
Michael <michaelcruz552@gmail.com>
2026-04-13 06:58:05 +0800

    refactor: transfer input handling to a separate hook

--- frontend/src/components/Length.tsx
+++ frontend/src/components/Length.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import useHandleInput from "../hooks/useHandleInput";
 
 const BASE_URL = "http://127.0.0.1:8000"
 
@@ -10,119 +11,57 @@ interface LengthProps {
 }
 
 function Length({ itemSelected, defaultFromUnit, defaultToUnit, unitOptions }: LengthProps) {
-    // manual update of value1 and value2 to prevent loop of useEffect calls
-    const [value1, setValue1] = useState("0");
-    const [value2, setValue2] = useState("0");
-    // track which input is being updated to prevent loop of useEffect calls
-    const [updatingInput, setUpdatingInput] = useState(0);
-    // state for fromUnit and toUnit to trigger useEffect when they change
-    const [fromUnitState, setFromUnitState] = useState(defaultFromUnit);
-    const [toUnitState, setToUnitState] = useState(defaultToUnit);
-
-    const inputRegex = /^\s*[-+]?\d+(\.\d+)?\s*$/;
-
-    const handleLength = (e: React.ChangeEvent<HTMLInputElement>, lengthSetState: string) => {
-        let inputValue = e.target.value;
-        if (lengthSetState === "value1") {
-            setValue1(inputValue);
-            setUpdatingInput(0);
-        } else {
-            setValue2(inputValue);
-            setUpdatingInput(1);
-        }
-    }
-
-    // Change setUpdatingInput based on desired behavior: if you want to update the other input when one changes, set it to the opposite value (0 or 1). If you want to update the same input, keep it the same.
-    const handleUnitChange = (e: React.ChangeEvent<HTMLSelectElement>, unitSetState: string) => {
-        let unitValue = e.target.value;
-        if (unitSetState === "setFromUnitState") {
-            setFromUnitState(unitValue);
-            setUpdatingInput(updatingInput);
-        } else {
-            setToUnitState(unitValue);
-            setUpdatingInput(updatingInput);
-        }
-    }
-
-    useEffect(() => {
-        const convertLength = async () => {
-            if (itemSelected === "Length" && updatingInput === 0 && inputRegex.test(value1)) {
-                try {
-                    const response = await fetch(`${BASE_URL}/length/convert?from_unit=${fromUnitState}&to_unit=${toUnitState}&value=${value1}`);
-                    const data = await response.json();
-                    setValue2(data.converted_value);
-                } catch (error) {
-                    console.error("Error converting length:", error);
-                } 
-            }
-        }
-        convertLength();
-    }, [itemSelected, fromUnitState, toUnitState, updatingInput, value1]);
-
-    useEffect(() => {
-        const convertLength = async () => {
-            if (itemSelected === "Length" && updatingInput === 1 && inputRegex.test(value2)) {
-                try {
-                    const response = await fetch(`${BASE_URL}/length/convert?from_unit=${toUnitState}&to_unit=${fromUnitState}&value=${value2}`);
-                    const data = await response.json();
-                    setValue1(data.converted_value);
-                } catch (error) {
-                    console.error("Error converting length:", error);
-                } 
-            }
-        }
-        convertLength();
-    }, [itemSelected, fromUnitState, toUnitState, updatingInput, value2]);
+    const { value1, value2, fromUnitState, toUnitState, handleInput, handleUnitChange } = useHandleInput(defaultFromUnit, defaultToUnit, itemSelected);
 
     return (
-    <div>
-        {itemSelected === "Length" && (
-            <>
-                <h2>Length Converter</h2>
-                <form className="row g-3">
-                    <div className="col">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder=""
-                            value={value1}
-                            onChange={(e) => handleLength(e, "value1")}
-                            id="input1"
-                        />
-                    </div>
-                    <div className="col">
-                        <select className="form-select" value={fromUnitState} onChange={(e) => handleUnitChange(e, "setFromUnitState")}>
-                            {unitOptions.map((unit) => (
-                                <option key={unit} value={unit}>
-                                    {unit.charAt(0).toUpperCase() + unit.slice(1)}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="col">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder=""
-                            value={value2}
-                            onChange={(e) => handleLength(e, "value2")}
-                            id="input2"
-                        />
-                    </div>
-                    <div className="col">
-                        <select className="form-select" value={toUnitState} onChange={(e) => handleUnitChange(e, "setToUnitState")}>
-                            {unitOptions.map((unit) => (
-                                <option key={unit} value={unit}>
-                                    {unit.charAt(0).toUpperCase() + unit.slice(1)}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                </form>
-            </>
-        )}
-    </div>
-  );
+        <div>
+            {itemSelected === "Length" && (
+                <>
+                    <h2>Length Converter</h2>
+                    <form className="row g-3">
+                        <div className="col">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder=""
+                                value={value1}
+                                onChange={(e) => handleInput(e, "value1")}
+                                id="input1"
+                            />
+                        </div>
+                        <div className="col">
+                            <select className="form-select" value={fromUnitState} onChange={(e) => handleUnitChange(e, "setFromUnitState")}>
+                                {unitOptions.map((unit) => (
+                                    <option key={unit} value={unit}>
+                                        {unit.charAt(0).toUpperCase() + unit.slice(1)}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="col">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder=""
+                                value={value2}
+                                onChange={(e) => handleInput(e, "value2")}
+                                id="input2"
+                            />
+                        </div>
+                        <div className="col">
+                            <select className="form-select" value={toUnitState} onChange={(e) => handleUnitChange(e, "setToUnitState")}>
+                                {unitOptions.map((unit) => (
+                                    <option key={unit} value={unit}>
+                                        {unit.charAt(0).toUpperCase() + unit.slice(1)}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    </form>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default Length;
---
+++ frontend/src/hooks/useHandleInput.tsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+
+export default function useHandleInput(defaultFromUnit: string, defaultToUnit: string, itemSelected: string) {
+    const BASE_URL = "http://127.0.0.1:8000"
+    // manual update of value1 and value2 to prevent loop of useEffect calls
+    const [value1, setValue1] = useState("0");
+    const [value2, setValue2] = useState("0");
+    // state for fromUnit and toUnit to trigger useEffect when they change
+    const [fromUnitState, setFromUnitState] = useState(defaultFromUnit);
+    const [toUnitState, setToUnitState] = useState(defaultToUnit);
+    const [updatingInput, setUpdatingInput] = useState(0);
+
+    const inputRegex = /^\s*[-+]?\d+(\.\d+)?\s*$/;
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>, inputSetState: string) => {
+        let inputValue = e.target.value;
+        if (inputSetState === "value1") {
+            setValue1(inputValue);
+            setUpdatingInput(0);
+        } else {
+            setValue2(inputValue);
+            setUpdatingInput(1);
+        }
+    }
+
+    const handleUnitChange = (e: React.ChangeEvent<HTMLSelectElement>, unitSetState: string) => {
+        let unitValue = e.target.value;
+        if (unitSetState === "setFromUnitState") {
+            setFromUnitState(unitValue);
+            setUpdatingInput(updatingInput);
+        } else {
+            setToUnitState(unitValue);
+            setUpdatingInput(updatingInput);
+        }
+    }
+
+    useEffect(() => {
+        const convertLength = async () => {
+            if (itemSelected === "Length" && updatingInput === 0 && inputRegex.test(value1)) {
+                try {
+                    const response = await fetch(`${BASE_URL}/length/convert?from_unit=${fromUnitState}&to_unit=${toUnitState}&value=${value1}`);
+                    const data = await response.json();
+                    setValue2(data.converted_value);
+                } catch (error) {
+                    console.error("Error converting length:", error);
+                } 
+            }
+        }
+        convertLength();
+    }, [itemSelected, fromUnitState, toUnitState, updatingInput, value1]);
+
+    useEffect(() => {
+        const convertLength = async () => {
+            if (itemSelected === "Length" && updatingInput === 1 && inputRegex.test(value2)) {
+                try {
+                    const response = await fetch(`${BASE_URL}/length/convert?from_unit=${toUnitState}&to_unit=${fromUnitState}&value=${value2}`);
+                    const data = await response.json();
+                    setValue1(data.converted_value);
+                } catch (error) {
+                    console.error("Error converting length:", error);
+                } 
+            }
+        }
+        convertLength();
+    }, [itemSelected, fromUnitState, toUnitState, updatingInput, value2]);
+
+    return { value1, value2, fromUnitState, toUnitState, handleInput, handleUnitChange };
+}
